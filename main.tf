0745c22bee6ca8bd538be537ee87915928d2d959
assareh <assareh@hashicorp.com>
2020-02-19 20:56:27 -0800

    initial commit

---
+++ main.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_version = ">= 0.11.11"
+}
+
+// provider "vault" {
+//   address = "${var.vault_addr}"
+// }
+
+// data "vault_generic_secret" "gcp_credentials" {
+//   path = "secret/${var.vault_user}/gcp/credentials"
+// }
+
+provider "google" {
+  credentials = "${var.gcp_credentials}"
+  project     = "${var.gcp_project}"
+  region      = "${var.gcp_region}"
+}
+
+resource "google_container_cluster" "k8sexample" {
+  name                    = "k8s-cluster"
+  description             = "example k8s cluster"
+  location                = "${var.gcp_zone}"
+  initial_node_count      = "${var.initial_node_count}"
+  enable_kubernetes_alpha = "true"
+  enable_legacy_abac      = "true"
+  resource_labels         = var.labels
+
+  master_auth {
+    username = "${var.master_username}"
+    password = "${var.master_password}"
+
+    client_certificate_config {
+      issue_client_certificate = true
+    }
+  }
+
+  node_config {
+    machine_type = "${var.node_machine_type}"
+    disk_size_gb = "${var.node_disk_size}"
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring"
+    ]
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,64 @@
+variable "gcp_credentials" {
+  description = "GCP credentials needed by google provider"
+}
+
+variable "gcp_region" {
+  description = "GCP region, e.g. us-east1"
+  default     = "us-west1"
+}
+
+variable "gcp_zone" {
+  description = "GCP zone, e.g. us-east1-a"
+  default     = "us-west1-b"
+}
+
+variable "gcp_project" {
+  description = "GCP project name"
+}
+
+variable "initial_node_count" {
+  description = "Number of worker VMs to initially create"
+  default     = 1
+}
+
+variable "master_username" {
+  description = "Username for accessing the Kubernetes master endpoint"
+  default     = "k8smaster"
+}
+
+variable "master_password" {
+  description = "Password for accessing the Kubernetes master endpoint"
+  default     = "k8smasterk8smaster"
+}
+
+variable "node_machine_type" {
+  description = "GCE machine type"
+  default     = "f1-micro"
+}
+
+variable "node_disk_size" {
+  description = "Node disk size in GB"
+  default     = "10"
+}
+
+variable "environment" {
+  description = "value passed to Environment tag and used in name of Vault auth backend later"
+  default     = "gke-dev"
+}
+
+variable "labels" {
+  description = "descriptive labels for instances deployed"
+  default = {
+    "name" : "demo-compute-instance",
+    "owner" : "andy-assareh",
+    "ttl" : "1",
+  }
+}
+
+// variable "vault_user" {
+//   description = "Vault userid: determines location of secrets and affects path of k8s auth backend"
+// }
+
+// variable "vault_addr" {
+//   description = "Address of Vault server including port"
+// }
