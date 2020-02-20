09807ab5d381b8f4a1dc4a4a1086f9f1f9633987
assareh <assareh@hashicorp.com>
2020-02-19 22:13:56 -0800

    removed interpolation syntax and network to subnet per error

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 terraform {
-  required_version = ">= 0.11.11"
+  required_version = ">= 0.12"
 }
 
 provider "google" {
-  credentials = "${var.gcp_credentials}"
-  project     = "${var.gcp_project}"
-  region      = "${var.gcp_region}"
+  credentials = var.gcp_credentials
+  project     = var.gcp_project
+  region      = var.gcp_region
 }
 
 // provider "vault" {
@@ -30,25 +30,25 @@ data "terraform_remote_state" "network" {
 resource "google_container_cluster" "k8sexample" {
   name               = format("%s-%s", "k8s-cluster", var.env)
   description        = "example k8s cluster"
-  location           = "${var.gcp_zone}"
-  initial_node_count = "${var.initial_node_count}"
+  location           = var.gcp_zone
+  initial_node_count = var.initial_node_count
   enable_legacy_abac = "true"
   resource_labels    = var.labels
 
   master_auth {
-    username = "${var.master_username}"
-    password = "${var.master_password}"
+    username = var.master_username
+    password = var.master_password
 
     client_certificate_config {
       issue_client_certificate = true
     }
   }
 
-  network = "assareh-gke"
+  network = "subnet-01"
 
   node_config {
-    machine_type = "${var.node_machine_type}"
-    disk_size_gb = "${var.node_disk_size}"
+    machine_type = var.node_machine_type
+    disk_size_gb = var.node_disk_size
     oauth_scopes = [
       "https://www.googleapis.com/auth/compute",
       "https://www.googleapis.com/auth/devstorage.read_only",
