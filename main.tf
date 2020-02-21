7b6c30a89121589800891c077936dc31d3685c4e
Andy Assareh <assareh@hashicorp.com>
2020-02-20 16:04:22 -0800

    node count to 3

--- main.tf
+++ main.tf
@@ -107,7 +107,7 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
   name       = "my-node-pool"
   location   = var.gcp_zone
   cluster    = google_container_cluster.default.name
-  node_count = 20
+  node_count = 3
 
   node_config {
     preemptible  = true
